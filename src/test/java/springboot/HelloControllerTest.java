8fcb53ad1f89b6585e56e9eb9559e878e1a183ae
YSEO <1park4170@gmail.com>
2022-08-29 16:43:41 +0900

    add lombok

--- src/test/java/springboot/HelloControllerTest.java
+++ src/test/java/springboot/HelloControllerTest.java
@@ -7,10 +7,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import springboot.web.HelloController;
+import static org.hamcrest.Matchers.is;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class) // JUnit에 내장된 실행자 외에 다른 실행자 실행시킴, 스프링 부트 테스트와 JUnit 사이 연결자 역할
 @WebMvcTest(controllers = HelloController.class)
@@ -25,5 +25,19 @@ public class HelloControllerTest {
 
         mvc.perform(get("/hello")).andExpect(status().isOk()).andExpect(content().string(hello));
         /* /hello로 GET요청을 보내어, 응답이 200인지, 응답 내용이 hello인지 검증 */
+    }
+
+    @Test
+    public void helloDto_return() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                        get("/hello/dto")
+                                .param("name", name)
+                                .param("amount", String.valueOf(amount))) // param 값은 String만 허용되어서 문자열로 변경해야함
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 }
---
+++ src/test/java/springboot/dto/HelloResponseDtoTest.java
@@ -0,0 +1,24 @@
+package springboot.dto;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import springboot.web.dto.HelloResponseDto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void lombok_test() {
+        //given
+        String name = "test";
+        int amount = 1000;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        //then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
