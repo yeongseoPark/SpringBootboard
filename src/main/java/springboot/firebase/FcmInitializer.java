9141c2da982ca6e5fe5782c2e31f6a425c26136e
YSEO <1park4170@gmail.com>
2022-10-05 16:14:08 +0900

    alert 구현/ serviceworker화면 나타나는것, 404 해결 필요

---
+++ src/main/java/springboot/firebase/FcmInitializer.java
@@ -0,0 +1,33 @@
+package springboot.firebase;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+
+@Service
+public class FcmInitializer {
+    private static final Logger logger = LoggerFactory.getLogger(FcmInitializer.class);
+    private static final String FIREBASE_CONFIG_PATH = "webpush-coin-firebase-adminsdk-2eytt-04a22bac7d.json";
+
+    @PostConstruct
+    public void initialize() {
+        try {
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(new ClassPathResource(FIREBASE_CONFIG_PATH).getInputStream())).build();
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+                logger.info("Firebase application has been initialized");
+            }
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+}
---
+++ src/main/java/springboot/firebase/NotificationService.java
@@ -0,0 +1,43 @@
+package springboot.firebase;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+@Service
+public class NotificationService {
+
+    private static final Logger logger = LoggerFactory.getLogger(NotificationService.class);
+
+    private final FcmService fcmService;
+    private final Map<String, String> tokenMap = new HashMap<>();
+
+    public NotificationService(final FcmService fcmService) {
+        this.fcmService = fcmService;
+    }
+
+    public void register(final String userEmail, final String token) {
+        tokenMap.put(userEmail, token);
+    }
+
+    public void deleteToken(final String userEmail) {
+        tokenMap.remove(userEmail);
+    }
+
+    public String getToken(final String userEmail) {
+        return tokenMap.get(userEmail);
+    }
+
+    public void sendNotification(final NotificationRequest request) {
+        try {
+            System.out.println("토큰맵 : " + tokenMap);
+            fcmService.send(request);
+        } catch (InterruptedException | ExecutionException e) {
+            logger.error(e.getMessage());
+        }
+    }
+}
