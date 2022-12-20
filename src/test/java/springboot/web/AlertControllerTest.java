5013da77a69273af54284e456f85a601490983a6
YSEO <1park4170@gmail.com>
2022-12-20 20:37:02 +0900

    test<alertsSave_test>

--- src/test/java/springboot/web/AlertControllerTest.java
+++ src/test/java/springboot/web/AlertControllerTest.java
@@ -1,5 +1,6 @@
 package springboot.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.assertj.core.api.Assertions;
 import org.junit.Before;
 import org.junit.jupiter.api.AfterEach;
@@ -9,24 +10,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.servlet.view.MustacheViewResolver;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.ViewResolver;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import springboot.domain.alert.Alert;
+import springboot.domain.alert.AlertRepository;
+import springboot.domain.alert.AlertType;
 import springboot.domain.user.User;
 import springboot.domain.user.UserRepository;
+import springboot.web.dto.alert.alertSaveDto;
 
 import java.util.Locale;
 
@@ -42,6 +50,9 @@ public class AlertControllerTest {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private AlertRepository alertRepository;
 
     @Before
     public void setup() {
@@ -73,16 +84,43 @@ public class AlertControllerTest {
         Assertions.assertThat(body).contains("알림 등록");
     }
 
-
     @Test
+    @WithMockCustomUser
+    @Sql(statements = "INSERT INTO user (name, email, role)\n" +
+            "VALUES ('testName', 'testEmail@naver.com', 'USER');")
+    @Transactional
     public void AlertsSave_test() throws Exception {
+        User user = userRepository.findByEmail("testEmail@naver.com").get();
+        alertSaveDto saveDto = new alertSaveDto(user,100.1, 12.2, "bitcoin", AlertType.Lower_break.toString());
+
+        ObjectMapper mapper = new ObjectMapper();
+        String requestBody= mapper.writeValueAsString(saveDto);
+
+        mockMvc.perform(post("/alerts/save")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody))
+                .andExpect(status().isOk());
+
+        Assertions.assertThat(alertRepository.findByUser(user.getId()).get(0).getPrice()).isEqualTo(100.1);
 
     }
 
-    @Test
-    public void AlertUserbyPrice_test() {
-
-    }
+//    @Test
+//    @WithMockCustomUser
+//    @Sql(statements = "INSERT INTO user (name, email, role)\n" +
+//            "VALUES ('testName', 'testEmail@naver.com', 'USER');")
+//    public void AlertUserbyPrice_test() {
+//        User user = userRepository.findByEmail("testEmail@naver.com").get();
+//        alertSaveDto saveDto = new alertSaveDto(user,100.1, 12.2, "bitcoin", AlertType.Lower_break.toString());
+//
+//        alertController.AlertSave(saveDto);
+//
+//        Alert alert = alertRepository.findByUser(user.getId()).get(0);
+//        Long id = alert.getId();
+//
+//        mockMvc.perform(get("/alerts/" + id)
+//                .
+//    }
 
     @Test
     public void AlertUserbyPercentage_test() {
