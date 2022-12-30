1d073ae2b2f34466e0d39d8b13bceec34bedc12e
ec2 <1park4170@gmail.com>
2022-12-30 18:39:17 +0900

    ec2 env

--- build/resources/main/static/js/app/index.js
+++ build/resources/main/static/js/app/index.js
@@ -12,11 +12,60 @@ var main = {
         $('#btn-delete').on('click', function () {
             _this.delete();
         });
+
+        $('#btn-comment-save').on('click', function () {
+            _this.commentSave();
+        });
+
+        $('#alert-save-btn').on('click', function() {
+            _this.alertSave();
+        });
+
+       $('input[name=alert-start]').on('click', function(e) { //e는 input 정보를 가진 이벤트
+           _this.alertStart(e);
+       });
     },
+
+
+    alertStart : function(e) {
+        var id =  $(e.target).data("id");
+        // target은 이벤트가 발생한 대상 객체
+         $.ajax({
+                            type: 'GET',
+                            url: '/alerts/'+id,
+                        }).done(function() {
+                            alert('알림이 시작됩니다.');
+                        }).fail(function (error) {
+                            alert(JSON.stringify(error));
+                        });
+    },
+
+    alertSave : function () {
+        var data = {
+            price : $('#price').val(),
+            percentage : $('#percentage').val(),
+            ticker : $('#contact').val(),
+            alertType : $('#alertType').val()
+        };
+
+        $.ajax({
+                    type: 'POST',
+                    url: '/alerts/save',
+                    dataType: 'json',
+                    contentType:'application/json; charset=utf-8',
+                    data: JSON.stringify(data)
+                }).done(function() {
+                    alert('알림이 등록되었습니다.');
+                    window.location.href = '/alerts'; // 글 등록이 성공하면 메인페이지로 이동
+                }).fail(function (error) {
+                    alert(JSON.stringify(error));
+                });
+    },
+
     save : function () {
         var data = {
             title: $('#title').val(),
-            author: $('#author').val(),
+//            author: $('#author').val(),
             content: $('#content').val()
         };
 
@@ -28,7 +77,7 @@ var main = {
             data: JSON.stringify(data)
         }).done(function() {
             alert('글이 등록되었습니다.');
-            window.location.href = '/';
+            window.location.href = '/'; // 글 등록이 성공하면 메인페이지로 이동
         }).fail(function (error) {
             alert(JSON.stringify(error));
         });
@@ -54,6 +103,7 @@ var main = {
             alert(JSON.stringify(error));
         });
     },
+
     delete : function () {
         var id = $('#id').val();
 
@@ -68,8 +118,32 @@ var main = {
         }).fail(function (error) {
             alert(JSON.stringify(error));
         });
-    }
+    },
 
+    commentSave : function () {
+        var data = {
+            postsId: $('#postsId').val(),
+            comment: $('#comment').val()
+        }
+        // 공백 및 빈 문자열 체크
+        if (!data.comment || data.comment.trim() === "") {
+             alert("공백 또는 입력하지 않은 부분이 있습니다.");
+             return false;
+        } else {
+            $.ajax({
+                 type: 'POST',
+                 url: '/api/v1/posts/' + data.postsId + '/comments',
+                 dataType: 'JSON',
+                 contentType: 'application/json; charset=utf-8',
+                 data: JSON.stringify(data)
+                 }).done(function () {
+                        alert('댓글이 등록되었습니다.');
+                        window.location.reload();
+                 }).fail(function (error) {
+                    alert(JSON.stringify(error));
+                 });
+             }
+     }
 };
 
 main.init();
